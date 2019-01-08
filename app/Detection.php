b0c0a765faf76176b9e18af888c9f910887139e6
Simon Månsson <simon.mansson@niftitech.com>
2019-01-08 20:40:08 +0100

    Removed some code that was not used from detection model.

--- app/Detection.php
+++ app/Detection.php
@@ -15,19 +15,6 @@ class Detection extends Model
     protected $fillable = ['geom', 'project_id'];
 
     /**
-     * Boot method
-     * 
-     * @return void
-     */
-    public static function boot()
-    {
-        parent::boot();
-        static::creating(function($model) {
-            $model->setAttribute('geom',  \DB::raw('ST_GeomFromGeoJSON("{\'type\':\'Point\',\'coordinates\':[-48.23456,20.12345]}")') );
-        });
-    }
-
-    /**
      * Get the project that this label belongs to.
      */
     public function project()
