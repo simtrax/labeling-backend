205f11bfe4121cbbd96d601bf757bf87eb330474
Simon Månsson <simon.mansson@niftitech.com>
2019-01-22 21:37:03 +0100

    Can now run basic darknet detection. Still some work to be done.

--- app/Jobs/RunDarknetDetectionJob.php
+++ app/Jobs/RunDarknetDetectionJob.php
@@ -11,12 +11,14 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
 
 use App\Project;
+use App\YoloModel;
 
 class RunDarknetDetectionJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $project;
+    public $model;
     public $jobPath;
 
     /**
@@ -24,9 +26,10 @@ class RunDarknetDetectionJob implements ShouldQueue
      *
      * @return void
      */
-    public function __construct(Project $project)
+    public function __construct(Project $project, YoloModel $model)
     {
         $this->project = $project;
+        $this->model = $model;
     }
 
     /**
@@ -36,31 +39,32 @@ class RunDarknetDetectionJob implements ShouldQueue
      */
     public function handle()
     {
-        $jobFolderName = (string) \Uuid::generate(4);
-
-        $this->jobPath = Storage::makeDirectory('detection/running_jobs/' . $jobFolderName);
-
-        // Move needed files to local folder
-
-        $filePath = Storage::disk('projects')->path($this->project->geotif);
-        $outputFolderPath = Storage::disk('projects')->path($this->project->path . "/tiles");
+        $this->jobPath = 'detection/running_jobs/' . (string) \Uuid::generate(4);
+        
+        Storage::makeDirectory($this->jobPath);
+        
+        $this->copyConfigFiles();
+        $this->copyTifAndModel();
+        
+        $stream = Storage::getDriver()->readStream('detection/darknet_binary/darknet');
+        Storage::put($this->jobPath . '/darknet', $stream);
+        $darknetBinaryPath = storage_path() . '/app/' . $this->jobPath . '/darknet';
+        chmod($darknetBinaryPath, 755);
+        
+        $this->copyPythonFiles();
 
         $this->project->update(['status' => 'processing']);
 
-        // $numCores = env('NUM_PROCESSING_CORES', '2');
-        
-        // exec("gdal2tiles.py {$filePath} {$outputFolderPath} -z {$this->project->minZoom}-{$this->project->maxZoom} --processes={$numCores} -s EPSG:32633", $out);
-        
+        exec('cd ' . storage_path() . '/app/' . $this->jobPath . ' && python2 darknet.py --image orthophoto.tif', $out);
+
         if($out) {
-            Storage::disk('projects')->delete([
-                $this->project->path . "/tiles/googlemaps.html",
-                $this->project->path . "/tiles/leaflet.html",
-                $this->project->path . "/tiles/openlayers.html",
-                $this->project->path . "/tiles/tilemapresource.xml",
-            ]);
+            dd($out);
         }
 
         $this->project->update(['status' => 'finished']);
+
+        unlink($darknetBinaryPath);
+        Storage::deleteDirectory($this->jobPath);
     }
 
     /**
@@ -79,4 +83,41 @@ class RunDarknetDetectionJob implements ShouldQueue
         Log::error($exception->getMessage());
     }
 
+    /**
+     * Move the needed config files for detection
+     */
+    public function copyConfigFiles()
+    {
+        $cfg = Storage::disk('projects')->get($this->project->path . '/cfg/yolo.cfg');
+        $data = Storage::disk('projects')->get($this->project->path . '/data/obj.data');
+        $names = Storage::disk('projects')->get($this->project->path . '/data/obj.names');
+
+        Storage::put($this->jobPath . '/cfg/yolo.cfg', $cfg);
+        Storage::put($this->jobPath . '/data/obj.data', $data);
+        Storage::put($this->jobPath . '/data/obj.names', $names);
+    }
+
+    /**
+     * Move the needed Orthophoto and the pre-trained yolo model
+     */
+    public function copyTifAndModel()
+    {
+        $stream = Storage::disk('projects')->getDriver()->readStream($this->model->path);
+        Storage::put($this->jobPath . '/model.weights', $stream);
+
+        $stream = Storage::disk('projects')->getDriver()->readStream($this->project->geotif);
+        Storage::put($this->jobPath . '/orthophoto.tif', $stream);
+    }
+
+    /**
+     * Find all the files in the sliding window project folder and copy them to the job folder
+     */
+    public function copyPythonFiles()
+    {
+        $files = array_diff(scandir(storage_path() . '/app/' . 'detection/sliding_window_darknet'), ['.', '..', '.gitignore', '.git', 'readme.md']);
+
+        foreach ($files as $file) {
+            Storage::put($this->jobPath . '/' . $file, Storage::get('detection/sliding_window_darknet/' . $file));
+        }
+    }
 }
